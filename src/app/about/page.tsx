487cb572a72ae46f854bdf1274379a713429a9bf
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2025-05-07 10:40:54 -0300

    feat: implement page about

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+import Modal from "../components/Modal"
+
+export default function About() {
+  return (
+    <> 
+    <Modal/> 
+    <div>
+      <h1>Sobre nós</h1>
+      <p>Bem-vindo à nossa página sobre nós!</p>
+      <p>Estamos aqui para ajudar você a entender mais sobre nossos serviços.</p>
+    </div>
+    </>
+  );
+}
