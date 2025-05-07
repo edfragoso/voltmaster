445008ba4068fb4d1ccb3f98704152e0515b277d
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2025-05-07 11:48:01 -0300

    fix: add new setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  compiler: {
+    styledComponents: true, // Ativa a compatibilidade com styled-components
+  },
 };
 
 export default nextConfig;
