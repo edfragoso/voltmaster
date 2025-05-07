60df05b69550dd4137b93a073a6c029399203367
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2025-05-07 10:10:23 -0300

    feat: implement new style comp

---
+++ src/app/components/Footer.style.ts
@@ -0,0 +1,16 @@
+'use client';
+import styled from 'styled-components';
+
+export const FooterContainer = styled.div`
+  max-width: auto;
+  margin: 0 auto;
+  padding: 0 2rem;
+
+  footer {
+    background-color: var(--dark);
+    color: white;
+    text-align: center;
+    padding: 2rem 0;
+    margin-top: 3rem;
+  }
+`;
