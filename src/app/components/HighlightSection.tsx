c35e2023c53d8464331c80ac112e30306ac46c43
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2025-05-07 10:12:22 -0300

    feat: implement new component

---
+++ src/app/components/HighlightSection.tsx
@@ -0,0 +1,11 @@
+'use client';
+import { HighlightContainer } from './HighlightSection.style';
+
+export default function HighlightSection() {
+  return (
+    <HighlightContainer>
+      <h2>Por que contratar um autônomo com 20 anos de experiência?</h2>
+      <p>Enquanto um CLT cumpre ordens, eu resolvo problemas. Sem intermediários, sem burocracias e sem custos ocultos. Você merece um eletricista que coloca a mão na massa (e nos fios) com compromisso real.</p>
+    </HighlightContainer>
+  );
+}
