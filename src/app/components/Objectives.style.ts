8079076df6aaa95459095f255085784b0b37b534
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2025-05-07 10:13:21 -0300

    feat: implement new component styled

---
+++ src/app/components/Objectives.style.ts
@@ -0,0 +1,16 @@
+'use client';
+import styled from 'styled-components';
+
+export const ObjectivesContainer = styled.section`
+  border-left: 5px solid var(--accent);
+  padding-left: 1.5rem;
+  margin: 3rem 0;
+
+  ol {
+    padding-left: 1rem;
+  }
+
+  li {
+    margin-bottom: 1rem;
+  }
+`;
