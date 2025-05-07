39c4f685661878ea82a606270c32d8b2065f7235
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2025-05-07 10:10:50 -0300

    feat: implement new component

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { FooterContainer } from "../components/Footer.style";
+
+export default function Footer() {
+  return (
+    <FooterContainer>
+      <footer>
+        <p>VoltMaster - Eletricista de Elite</p>
+        <p>
+          "Precisão, Tecnologia e Qualidade Superior – Soluções Elétricas que
+          Fazem a Diferença!"
+        </p>
+        <p>© 2023 | 20 anos iluminando ideias e energizando soluções</p>
+      </footer>
+    </FooterContainer>
+  );
+}
